f40e4a520ccff364fa1d46091be9d19eded89c69
morgunovvb <74514540+morgunovvb@users.noreply.github.com>
2022-10-25 20:17:52 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,12 +6,12 @@ from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 v1_router = DefaultRouter()
 
-router.register('posts', PostViewSet, basename='posts')
-router.register('posts/(?P<post_id>\d+)/comments', CommentViewSet,
+v1_router.register('posts', PostViewSet, basename='posts')
+v1_router.register('posts/(?P<post_id>\d+)/comments', CommentViewSet,
                 basename='comments')
-router.register('groups', GroupViewSet, basename='groups')
+v1_router.register('groups', GroupViewSet, basename='groups')
 
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(v1_router.urls)),
 ]
