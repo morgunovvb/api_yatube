bc3dcd625d5e43debbba53484c0017ed27962473
morgunovvb <74514540+morgunovvb@users.noreply.github.com>
2022-10-25 18:32:18 +0300

    Update urls.py

--- yatube_api/posts/urls.py
+++ yatube_api/posts/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
 ]
